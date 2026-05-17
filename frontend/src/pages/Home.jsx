fe18d3371608958cfba38ac4d22bc5b83e043a7f
Tidiane <tidianecs20pgmail.com>
2026-05-17 01:39:54 +0000

    Fix: Display bug of popular destinations

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
 
   useEffect(() => {
     getDestinations()
-      .then(res => setDestinations(res.data.slice(0, 6))) // 6 premières
+       .then(res => setDestinations(res.data.slice(0, 3)))
       .catch(console.error)
       .finally(() => setLoading(false))
   }, [])
@@ -66,8 +66,8 @@ export default function Home() {
         </div>
 
         {loading ? (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {[...Array(3)].map((_, i) => (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {[...Array(6)].map((_, i) => (
                 <div key={i} className="card h-72 animate-pulse">
                 <div className="h-48 bg-dark-hover rounded-lg mb-4" />
                 <div className="h-4 bg-dark-hover rounded w-3/4 mb-2" />
